4dfb41357f123e8d1570a021acea86c5ad4ed663
AbhishekChapagai <57378876+AbhishekChapagai@users.noreply.github.com>
2022-06-14 10:53:54 +0545

    first commit

---
+++ src/component/Navbar/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Logo from '../../assets/Images/travelLogo.png'
+
+const Header = () => {
+    return (
+        <>
+            <div className='main-container'>
+                <div className='menu_container'>
+                    <div className='menu_links'>
+                        <ul className='links_ul'>
+                            <li>Home</li>
+                            <li>Destination</li>
+                            <li>Hire Guide</li>
+                            <li>Blog</li>
+                        </ul>
+                    </div>
+                    <div className='logo'>
+                        <img src={Logo}/>
+                    </div>
+                    <div className='btn_login_signup'>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header
